58fde0ab907acef0d35b7aa9b6c7ace900f4d619
Tomasz Nowacki <tomasz.nowacki@poznan.merito.pl>
2023-12-10 13:15:54 +0100

    usuwanie posta

--- resources/views/posty/index.blade.php
+++ resources/views/posty/index.blade.php
@@ -13,6 +13,7 @@ Lista postów
         <th scope="col">Tytuł</th>
         <th scope="col">Autor</th>
         <th scope="col">Czas utworzenia</th>
+        <th scope="col">Akcja</th>
       </tr>
     </thead>
     <tbody>
@@ -23,11 +24,18 @@ Lista postów
         <td><a href="{{ route('posty.show',$post->id)}}">{{ $post['tytul'] }}</a></td>
         <td>{{ $post->autor }}</td>
         <td>{{ date('j F Y H:i:s', strtotime($post->created_at)) }}</td>
+        <td class="d-flex flex-row"><a href="{{ route('posty.edit', $post->id) }}"><button class="btn btn-success m-1" type="button">E</button></a>
+          <form class="form-inline" action="{{ route('posty.destroy', $post->id) }}" method="post">
+          @csrf
+          @method('DELETE')
+          <button class="btn btn-danger m-1" type="submit">X</button>
+        </form>
+        </td>
       </tr>
       @endforeach
       @else
       <tr>
-        <th scope="row" colspan="4">Nie ma żadnych postów</th>
+        <th scope="row" colspan="5">Nie ma żadnych postów</th>
       </tr>
       @endif
     </tbody>
